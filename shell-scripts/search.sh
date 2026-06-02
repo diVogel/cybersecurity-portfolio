c53cca8de0de227aa245e7198afb959ff2b4f231
diVogel <dinis6095@gmail.com>
2026-06-02 18:30:16 +0500

    my first script!

---
+++ shell-scripts/search.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+dir = " "
+text = " "
+filetype = " "
+
+echo "directory: "
+read dir 
+echo "text: "
+read text
+echo "filetype: "
+read filetype
+
+echo "Text search in: $dir in process "
+
+for file in "$dir"/*."$filetype";do 
+
+if grep -q "$text" "$file";then 
+echo "Result: $text store in $file" 
+fi
+
+done 
