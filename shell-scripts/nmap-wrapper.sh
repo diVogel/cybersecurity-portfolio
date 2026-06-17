625b686fcad6880639b4a29007c91bbee239c5d4
diVogel <dinis6095@gmail.com>
2026-06-17 17:57:50 +0500

    new note: Transport layer

--- shell-scripts/nmap-wrapper.sh
+++ shell-scripts/nmap-wrapper.sh
@@ -3,9 +3,15 @@
 domain=""
 
 echo "Please, write the domain: "
-read domain 
+read domain
 
-nslookup domain\
-| grep -i "server"\
-| IP= awk '{print $2}'\
-| nmap -sS $IP
+touch iplist.txt
+
+nslookup $domain\
+| grep -oP '\b(?:\d{1,3}\.){3}\d{1,3}\b'\
+| echo  >> iplist.txt
+
+#for ip in iplist.txt;do
+#nmap -sT $ip
+#done
+
